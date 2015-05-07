1fc53144123ec97805772b4f06f125f3fe6ddd07
Enrique <enrique.gib@gmail.com>
2015-05-07 16:01:59 +0200

    test

--- app/src/main/java/app/bqeestec/bqeestec/MainActivity.java
+++ app/src/main/java/app/bqeestec/bqeestec/MainActivity.java
@@ -1,8 +1,7 @@
 package app.bqeestec.bqeestec;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
 
@@ -14,13 +13,14 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
